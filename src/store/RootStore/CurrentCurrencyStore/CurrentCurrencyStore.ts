39cfa6c4ce26069856efb141c12d3a1c85cced0f
Nikolay Kovalev <nikkov13@gmail.com>
2022-09-03 02:17:37 +0300

    implemented saving current currency in sessionStorage

--- src/store/RootStore/CurrentCurrencyStore/CurrentCurrencyStore.ts
+++ src/store/RootStore/CurrentCurrencyStore/CurrentCurrencyStore.ts
@@ -4,7 +4,10 @@ import { computed, makeObservable, observable } from "mobx";
 type PrivateFields = "_currency";
 
 export default class CurrenciesStore {
-  private _currency: string = DEFAULT_CURRENCY;
+  private _currency: string = (() => {
+    const currency = sessionStorage.getItem("currency");
+    return currency ? currency : DEFAULT_CURRENCY;
+  })();
 
   constructor() {
     makeObservable<CurrenciesStore, PrivateFields>(this, {
@@ -19,5 +22,6 @@ export default class CurrenciesStore {
 
   set currency(curr) {
     this._currency = curr;
+    sessionStorage.setItem("currency", curr);
   }
 }
--- src/store/RootStore/instance.ts
+++ src/store/RootStore/instance.ts
@@ -1,5 +1,7 @@
 import RootStore from "./RootStore";
 
+// eslint-disable-next-line no-console
+console.log("init rootStore");
 const rootStore = new RootStore();
 
 export default rootStore;
